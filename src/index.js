1a69cdd34036c8a1271999faa3a7dd411bf38863
Melissa Nguyen <nguyen.melissa23@gmail.com>
2017-06-16 10:44:49 -0700

    package`

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@
 import ReactDOM from "react-dom";
 import routes from "./app/config/routes";
 
-import './app.css'; 
-
 // Rendering our router to the "app" div in index.html
 ReactDOM.render(routes, document.getElementById("app"));
